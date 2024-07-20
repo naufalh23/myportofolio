b0853071c2b14236ffe7a0440a1599250a1071a0
Naufal Hesantra <n.hesantra12@gmail.com>
2024-07-20 12:58:44 +0700

    First

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div>Hello World!</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div className="flex my-8 px-[70px] justify-center items-center font-mono h-16 bg-teal-400">
+            Portofolio
+        </div>
+    )
+}
