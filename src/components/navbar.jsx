501a640c0674cc077e75fa2bb071d59603c926ec
Naufal Hesantra <n.hesantra12@gmail.com>
2024-07-21 20:54:45 +0700

    update 1

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,14 @@
 export default function Navbar() {
     return (
-        <div className="flex my-8 px-[70px] justify-center items-center font-mono h-16 bg-teal-400">
-            Portofolio
-        </div>
+        <header className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
+            <div className="container">
+                <div className="flex items-center justify-between relative">
+                    <div className="px-4">
+                        <a href="#home" className="font-bold text-lg text-primary block py-6">Portofolio</a>
+                    </div>
+                </div>
+            </div>
+
+        </header>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0872BF',
+        second: '#FFFFFF',
+        gradien: '#0D92AF',
+        boxproject: '#0C1010',
+        border1: '#0B467C',
+        border2: '#37536C',
+      }
+    },
   },
   plugins: [],
 }
