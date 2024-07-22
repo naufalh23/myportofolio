9275400fbf8dd963b45df072b47ab3be4542f834
Naufal Hesantra <n.hesantra12@gmail.com>
2024-07-22 22:15:43 +0700

    update2

---
+++ src/components/project.jsx
@@ -0,0 +1,41 @@
+export default function Project () {
+    return (
+        <section id="project" className="pt-20 px-[122px]">
+        <div className="container">
+            <div className="w-full px-4">
+                <div className="max-w-xl mx-auto text-center mb-16">
+                <h4 className="text-2xl font-semibold text-second">My Services</h4>
+                <h2 className="text-[40px] font-bold text-primary">What I Do</h2>
+                </div>
+            </div>
+            
+            
+            <div className="w-full mt-[71px] flex flex-wrap">
+                <div className="p-8 w-[354px] ring-2 ring-primary rounded-3xl mr-14">
+                    <div className="flex justify-center">
+                        <img src="src/img/k1.png" alt="Fullstack" width="195" className="mb-11 " />
+                    </div>
+                    <h3 className="text-center text-second text-2xl font-semibold mb-11">Fullstack Web Dev.</h3>
+                    <p className="text-xl text-second font-light text-center">strive to create visually impact designs that effectively communicate ,messages & meet their unique needs.</p>
+                </div>
+                <div className="p-8 w-[354px] ring-2 ring-primary rounded-3xl mr-14">
+                    <div className="flex justify-center">
+                        <img src="src/img/k1.png" alt="Fullstack" width="195" className="mb-11 " />
+                    </div>
+                    <h3 className="text-center text-second text-2xl font-semibold mb-11">Fullstack Web Dev.</h3>
+                    <p className="text-xl text-second font-light text-center">strive to create visually impact designs that effectively communicate ,messages & meet their unique needs.</p>
+                </div>
+                <div className="p-8 w-[354px] ring-2 ring-primary rounded-3xl mr-14">
+                    <div className="flex justify-center">
+                        <img src="src/img/k1.png" alt="Fullstack" width="195" className="mb-11 " />
+                    </div>
+                    <h3 className="text-center text-second text-2xl font-semibold mb-11">Fullstack Web Dev.</h3>
+                    <p className="text-xl text-second font-light text-center">strive to create visually impact designs that effectively communicate ,messages & meet their unique needs.</p>
+                </div>
+            </div>
+            
+              
+        </div>
+        </section>
+    )
+}
